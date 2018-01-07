0910cdda066bbdef6594cbc0331fe47865a1eb66
Orsan Awwad <orsan.1997@gmail.com>
2018-01-07 14:02:42 +0200

    Added more comments (still not done)

--- תרגיל 6/game.h
+++ תרגיל 6/game.h
@@ -10,9 +10,69 @@
 
 #include "person.h"
 
+/**************************************************************************
+ * Function name: InitTheHungerGame
+ * Input: No input
+ * Output: Returns a pointer to Person object which is the head of the
+ *         circle.
+ * The function operation: Asks the user if he wants to create Persons
+ *                         objects and proceed into allocating memory for
+ *                         each person object then asks the user for name,
+ *                         id, number of kids, and each kid's name,
+ *                         once the function is over it returns the "first"
+ *                         object which is actually the first node in the
+ *                         circle aka the head of circle.
+ *************************************************************************/
 Person* InitTheHungerGame();
+
+/**************************************************************************
+ * Function name: InsertLaters
+ * Input: Person pointer (The head of circle)
+ * Output: No output
+ * The function operation: Asks the user if there are any other persons
+ *                         that needs to be added and adds them just like
+ *                         InitTheHungerGames but this time it asks for ID
+ *                         of where to "append" the new person object
+ *                         and finds the pointer of that person with the
+ *                         input id, if found, it replaces the next
+ *                         pointer of that person and with the new person's
+ *                         next pointer is pointed at the what was
+ *                         previously pointed by the input id person
+ *************************************************************************/
 void InsertLaters(Person* head);
+
+/**************************************************************************
+ * Function name: RemoveCowards
+ * Input: Person pointer (The head of circle)
+ * Output: Returns a pointer to Person object which is the head of the
+ *         circle.
+ * The function operation: Asks the user if there are any persons to remove
+ *                         if there are, it asks for their ids, then looks
+ *                         for them in the circle, if found, they're
+ *                         removed and deallocated from memory, then the
+ *                         previous node of the coward and the one after
+ *                         it are connected (basicaly replacing the next
+ *                         pointer of the previous node of the coward to
+ *                         point to the one after it).
+ *************************************************************************/
 Person* RemoveCowards(Person* head);
+
+/**************************************************************************
+ * Function name: LetTheHungerGameBegin
+ * Input: Person pointer (The head of circle)
+ * Output: No output
+ * The function operation: Starts the hunger games, it starts from head,
+ *                         builds and prints the kill statement of who
+ *                         kills who, then calls the function killNext
+ *                         of the current person pHead pointer, which then
+ *                         kills the person sits next to the pHead person
+ *                         and deallocates him from memory, then it gets
+ *                         connected to the one after it, once all the
+ *                         nodes in the circle are down and there is only
+ *                         one left, display stayin' alive lyrics with
+ *                         the person's name who won the game, then kill
+ *                         that last person and deallocate him... yep...
+ *************************************************************************/
 void LetTheHungerGameBegin(Person* head);
 
 #endif
--- תרגיל 6/person.h
+++ תרגיל 6/person.h
@@ -8,7 +8,6 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-
 struct Person {
     char* name;
     int id;
@@ -23,8 +22,32 @@ struct Person {
 
 typedef struct Person Person;
 
-//use only once
+/**************************************************************************
+ * Function name: CreatePerson
+ * Input: No input
+ * Output: Returns pointer to a newly allocated person object
+ * The function operation: Allocates new person object and calls
+ *                         InitPersonValues to initialize all of it's
+ *                         values like name, id, and kids names if there's
+ *                         any, then returns the pointer to that object.
+ *************************************************************************/
 Person* CreatePerson();
+
+/**************************************************************************
+ * Function name: CreateNext
+ * Input: Person pointerz, IsNextNext boolean, variadic list of Person struct
+ *        pointers.
+ * Output: No output.
+ * The function operation: If IsNextNext is false, it takes the current
+ *                         person and creates new person in its next
+ *                         pointer, else, it takes the variadic list and
+ *                         extracts the person object from there, then
+ *                         it replaces the next pointer to of the
+ *                         original person object to point to a new person
+ *                         then in the new person next's pointer is then
+ *                         pointed to the person that was extracted from
+ *                         the variadic list.
+ *************************************************************************/
 void InitPersonValues(Person* person);
 
 #endif
